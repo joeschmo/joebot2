bd1b3543b04a9ec2bb1264caea9102a0017612d7
joeschmo <joseph.lee@originate.com>
2013-07-02 11:42:23 -0700

    added a basic quit command

--- src/Main.hs
+++ src/Main.hs
@@ -3,10 +3,19 @@ import PluginUtils
 import Mail.Base
 import Mail.Cmd
 
+import System.Exit
+import Control.Lens
+import Core
+
 main = do
   conf <- spawnProc defaultConfig
                     mailProc 
                     [mail, rcv, inbox]
                     [mHook]
                     []
-  joebot $ conf
+  joebot $ conf & cmds %~ ((:) quit)
+
+  quit = Command "!quit" 0 quit' "!quit"
+    where quit' _ _ _ = do
+            write "QUIT" ":Deactivated"
+            liftIO $ exitWith ExitSuccess
